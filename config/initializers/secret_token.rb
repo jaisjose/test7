6ad1fd04e91032b8b59867544e4961e03edce001
unknown <jais@zoowork.com>
2013-03-19 22:44:33 +0530

    my firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gocoruby1::Application.config.secret_token = 'a8e7cb7e18eccf387d4936274937f27321243746960a32a1e25d6621dbee457b5fb28fd00fd61f300577030f1e318689b3d9816aa89a7464a15e12aa7b85ac99'
